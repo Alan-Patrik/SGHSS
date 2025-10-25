0ca1fe087e41e761869e57f1229bfe4e7829be03
Alan-Patrik <alanpatrik.fragozo@gmail.com>
2025-10-25 11:42:57 -0300

    adicionando estrutura inicial da api

---
+++ sghss-api/src/main/java/com/alanpatrik/sghss/api/SghssApiApplication.java
@@ -0,0 +1,13 @@
+package com.alanpatrik.sghss.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SghssApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SghssApiApplication.class, args);
+	}
+
+}
---
+++ sghss-api/src/test/java/com/alanpatrik/sghss/api/SghssApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alanpatrik.sghss.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SghssApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
