6ce590003c9d8b45f40faf3c6ec1a80db669deca
Alan-Patrik <alanpatrik.fragozo@gmail.com>
2025-11-04 14:10:35 -0300

    adicionando configuração e documentação do Swagger

--- sghss-api/src/main/java/com/alanpatrik/sghss/api/SghssApiApplication.java
+++ sghss-api/src/main/java/com/alanpatrik/sghss/api/SghssApiApplication.java
@@ -1,8 +1,11 @@
 package com.alanpatrik.sghss.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@OpenAPIDefinition
 @SpringBootApplication
 public class SghssApiApplication {
 
--- sghss-api/src/main/java/com/alanpatrik/sghss/api/controller/PacienteController.java
+++ sghss-api/src/main/java/com/alanpatrik/sghss/api/controller/PacienteController.java
@@ -1,23 +1,34 @@
 package com.alanpatrik.sghss.api.controller;
 
+import com.alanpatrik.sghss.api.comum.Constantes;
 import com.alanpatrik.sghss.api.dto.request.PacienteRequestDTO;
 import com.alanpatrik.sghss.api.dto.response.PacienteResponseDTO;
+import com.alanpatrik.sghss.api.model.ErroDTO;
 import com.alanpatrik.sghss.api.service.PacienteService;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/pacientes")
 public class PacienteController {
 
-    @Autowired
-    private PacienteService pacienteService;
+    private final PacienteService pacienteService;
 
     @GetMapping
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = Constantes.OK_MESSAGE, useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "500", description = Constantes.ERRO_MESSAGE, content = @Content(schema = @Schema(implementation = ErroDTO.class)))
+    })
     public ResponseEntity<List<PacienteResponseDTO>> getAll() {
         return ResponseEntity
                 .status(HttpStatus.OK.value())
@@ -25,30 +36,46 @@ public class PacienteController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<PacienteResponseDTO> getById(@PathVariable Long id) throws Exception {
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = Constantes.OK_MESSAGE, useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "404", description = Constantes.NOT_FOUND_MESSAGE, content = @Content(schema = @Schema(implementation = ErroDTO.class))),
+            @ApiResponse(responseCode = "500", description = Constantes.ERRO_MESSAGE, content = @Content(schema = @Schema(implementation = ErroDTO.class)))
+    })
+    public ResponseEntity<PacienteResponseDTO> getById(
+            @Parameter(example = "id", description = "Id do paciente cadastrado", required = true) @PathVariable(name = "id") Long id
+    ) {
         return ResponseEntity
                 .status(HttpStatus.OK.value())
                 .body(pacienteService.findById(id));
     }
 
     @PostMapping
-    public ResponseEntity<PacienteResponseDTO> create(@RequestBody PacienteRequestDTO pacienteRequestDTO) throws Exception {
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = Constantes.CREATE_MESSAGE, useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "400", description = Constantes.BAD_REQUEST_MESSAGE, content = @Content(schema = @Schema(implementation = ErroDTO.class))),
+            @ApiResponse(responseCode = "500", description = Constantes.ERRO_MESSAGE, content = @Content(schema = @Schema(implementation = ErroDTO.class)))
+    })
+    public ResponseEntity<PacienteResponseDTO> create(
+            @Parameter(example = "Paciente", description = "Objeto Paciente", required = true, name = "pacienteRequestDTO") @RequestBody PacienteRequestDTO pacienteRequestDTO
+    ) {
         return ResponseEntity
                 .status(HttpStatus.CREATED.value())
                 .body(pacienteService.save(pacienteRequestDTO));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<PacienteResponseDTO> update(@PathVariable Long id, @RequestBody PacienteRequestDTO pacienteRequestDTO) throws Exception {
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = Constantes.OK_MESSAGE, useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "400", description = Constantes.BAD_REQUEST_MESSAGE, content = @Content(schema = @Schema(implementation = ErroDTO.class))),
+            @ApiResponse(responseCode = "404", description = Constantes.NOT_FOUND_MESSAGE, content = @Content(schema = @Schema(implementation = ErroDTO.class))),
+            @ApiResponse(responseCode = "500", description = Constantes.ERRO_MESSAGE, content = @Content(schema = @Schema(implementation = ErroDTO.class)))
+    })
+    public ResponseEntity<PacienteResponseDTO> update(
+            @Parameter(example = "id", description = "Id do paciente cadastrado", required = true) @PathVariable(name = "id") Long id,
+            @Parameter(example = "Paciente", description = "Objeto Paciente", required = true, name = "pacienteRequestDTO") @RequestBody PacienteRequestDTO pacienteRequestDTO
+    ) {
         return ResponseEntity
                 .status(HttpStatus.OK.value())
                 .body(pacienteService.update(id, pacienteRequestDTO));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity delete(@PathVariable Long id) throws Exception {
-        pacienteService.delete(id);
-        return ResponseEntity
-                .status(HttpStatus.NO_CONTENT.value()).build();
     }
 }
